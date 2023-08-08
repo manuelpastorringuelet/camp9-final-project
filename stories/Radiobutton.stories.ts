3e4ce58637bee1369224e9895b110331606f029f
Manuel Pastor <pastorringuelet@gmail.com>
2023-08-08 15:54:57 +0200

    fixed RadioButton args issue

--- stories/Radiobutton.stories.ts
+++ stories/Radiobutton.stories.ts
@@ -11,17 +11,5 @@ export default meta;
 type Story = StoryObj<typeof Radiobutton>;
 
 export const Primary: Story = {
-  args: {
-    variant: 'primary',
-  },
-};
-export const Secondary: Story = {
-  args: {
-    variant: 'secondary',
-  },
-};
-export const Tertiary: Story = {
-  args: {
-    variant: 'tertiary',
-  },
+  args: {},
 };
